a4454472649ef1384b41ea6e431147978ce42c8f
davidcoats <david@getsafetycone.com>
2023-01-25 20:20:58 +0530

    Interim changes.

--- source/R5T.S0043/Code/Operations/ISolutionOperations.cs
+++ source/R5T.S0043/Code/Operations/ISolutionOperations.cs
@@ -35,9 +35,9 @@ namespace R5T.S0043
 		public void AddNew_ConsoleProjectToSolution()
 		{
 			/// Inputs.
-			var solutionFilePath = @"C:\Code\DEV\Git\GitHub\SafetyCone\R5T.F0027\source\R5T.F0027.Construction.sln";
-			var projectName = "R5T.F0027.Construction";
-			var projectDescription = "Construction project for the dotnet executable functionality library.";
+			var solutionFilePath = @"C:\Code\DEV\Git\GitHub\SafetyCone\R5T.S0061\source\R5T.S0061.sln";
+			var projectName = "R5T.S0061.F001";
+			var projectDescription = "Miscellaneous scripts project for the main Fresno (functionality survey) script.";
 
 			/// Run.
 			// Create the project file.
@@ -64,8 +64,8 @@ namespace R5T.S0043
 		{
 			/// Inputs.
 			var solutionFilePath = @"C:\Code\DEV\Git\GitHub\SafetyCone\R5T.S0061\source\R5T.S0061.sln";
-			var projectName = "R5T.S0061.Z001";
-			var projectDescription = "Values for Fresno (R5T.S0061) script.";
+			var projectName = "R5T.S0061.F001";
+			var projectDescription = "Functionality library for the Fresno script.";
 
 			/// Run.
 			// Create the project file.
